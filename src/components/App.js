3a81c12447c7df53979a80c96e6e8c17c7c87452
AmanVerma5 <amanv4775@gmail.com>
2023-11-09 22:59:45 +0530

    submitting code-Thu Nov 09 2023 22:59:42 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const [search,setSearch]=useState('');
+  const [item,setItem]=useState(fruits);
+
+   function handleChange(e){
+    setSearch(e.target.value)
+    let arr=fruits.filter((fruit)=>{
+      return fruit.includes(e.target.value)
+    })
+    
+    setItem(arr);
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <h1>Search</h1>
+      <input type='text' value={search} onChange={handleChange}/>
+      <ul>
+      {
+        item.map((element)=>
+          <li>{element}</li>
+        )
+      }
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
